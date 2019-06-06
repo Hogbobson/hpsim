36818d1179e6d3a586654a86a25f0bae50475ccc
Hogbobson <hogbobson@gmail.com>
2019-06-06 09:25:21 +0200

    Trying hdf5

---
+++ hpsim/hdf5gen.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 11:18:52 2019
+
+@author: hogbobson
+"""
+
+import h5py
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
